dfc45abd5ac261a24f75cb3be438095598767a88
Mikhail Perevalov <mixei87@mail.ru>
2021-08-24 00:24:38 +0700

    don_t work %llu

---
+++ P02D13/include/hash.h
@@ -0,0 +1,9 @@
+#ifndef HASH_H
+#define HASH_H
+void create_hash();
+void create_h_arr(unsigned int *h_arr, int n);
+void create_k_arr(unsigned int *k_arr, int n);
+void create_bin_arr(unsigned long long **message);
+void create_arr_simple_num(unsigned int *h_arr, int n);
+unsigned int create_32bit_num(double num);
+#endif  // !HASH_H
---
+++ P02D13/src/hash.c
@@ -0,0 +1,98 @@
+#include "../include/hash.h"
+#define __USE_MINGW_ANSI_STDIO 1
+// #include <inttypes.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+void create_hash() {
+  int n = 8;
+  unsigned int h_arr[n];
+  create_h_arr(h_arr, n);
+  n = 64;
+  unsigned int k_arr[n];
+  create_k_arr(k_arr, n);
+  unsigned long long **message =
+      (unsigned long long **)malloc(1 * sizeof(unsigned long long *));
+  create_bin_arr(message);
+  free(message);
+}
+void create_bin_arr(unsigned long long **message) {
+  unsigned long long i = 0;
+  do {
+    message[i] = (unsigned long long *)malloc(1 * sizeof(unsigned long long));
+    message[i][0] = 0;
+
+    FILE *file;
+    char path[100] = "./message.txt";
+    if ((file = fopen(path, "r")) != NULL) {
+      while (!feof(file)) {
+        char ch = 0;
+        message[i][0] <<= 1;
+        fscanf(file, "%c", &ch);
+        message[i][0] += (int)ch;
+        printf("%llu ", message[i][0]);
+      }
+      fclose(file);
+    }
+    i++;
+  } while (i < 1);  // create dynamic arr[1][512]
+  for (unsigned long long k = 0; k < i; k++) {
+    free(message[k]);
+  }
+  // begin read matrix
+  // coding each symbol to binary format
+  // if len of matrix (in binary) < 448 then add 1 and puts zero to end of 447th
+  // count len matrix and write this at 64 bit
+  // add this 64bit to end of arr (from 448th to 511th)
+}
+void create_h_arr(unsigned int *h_arr, int n) {
+  // first 32 bit of fractional part square root of first 8 simple numbers [from
+  // 2 to 19]
+  create_arr_simple_num(h_arr, n);
+  for (int i = 0; i < n; i++) {
+    double num = sqrt(h_arr[i]);
+    num -= floor(num);
+    h_arr[i] = create_32bit_num(num);
+  }
+}
+void create_k_arr(unsigned int *k_arr, int n) {
+  // first 32 bit of fractional part cube root of first 64 simple numbers [from
+  // 2 to 311]
+  create_arr_simple_num(k_arr, n);
+  for (int i = 0; i < n; i++) {
+    double num = cbrt(k_arr[i]);
+    num -= floor(num);
+    k_arr[i] = create_32bit_num(num);
+  }
+}
+unsigned int create_32bit_num(double num) {
+  int i, n = 32;
+  unsigned int res = 0;
+  for (i = -1; i >= n * -1; i--) {
+    res <<= 1;
+    if (num / pow(2, i) > 1) {
+      res += 1;
+      num -= pow(2, i);
+    }
+  }
+  return res;
+}
+// n must be > 0
+void create_arr_simple_num(unsigned int *h_arr, int n) {
+  h_arr[0] = 2;
+  int num, flag_simple;
+  for (int i = 1; i < n; i++) {
+    num = h_arr[i - 1] + 1;
+    do {
+      flag_simple = 1;
+      for (int k = 0; k < i; k++) {
+        if (num % h_arr[k] == 0) {
+          num += 1;
+          flag_simple = 0;
+          break;
+        }
+      }
+    } while (flag_simple == 0);
+    h_arr[i] = num;
+  }
+}

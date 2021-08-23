7fb49e0ca4367dcaae450a1d6edc3988caeb492e
Mikhail Perevalov <mixei87@mail.ru>
2021-08-23 21:39:59 +0700

    Initial commit

--- P02D13/include/hash.h
+++
@@ -1,8 +0,0 @@
-#ifndef HASH_H
-#define HASH_H
-void create_hash();
-void create_h_arr();
-void create_k_arr();
-void create_bin_arr();
-void create_arr_simple_num(unsigned int *h_arr, int n);
-#endif  // !HASH_H

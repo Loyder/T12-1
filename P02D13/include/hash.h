76e5e8da0dcd99879304062f8ee5ed7b76d52cd0
Mikhail Perevalov <mixei87@mail.ru>
2021-08-29 12:23:01 +0700

    not complete: preparing_message

--- P02D13/include/hash.h
+++ P02D13/include/hash.h
@@ -1,11 +1,11 @@
 #ifndef HASH_H
 #define HASH_H
-#include <stddef.h>
+#include <stdio.h>
 void create_hash();
 void create_h_arr(unsigned *h_arr, int n);
 void create_k_arr(unsigned *k_arr, int n);
 void create_bin_arr(unsigned **message);
 void create_arr_simple_num(unsigned *h_arr, int n);
 unsigned int create_32bit_num(double num);
-void get_size_of_binary_file(char *path, long int *size, int *err);
+void get_size_of_binary_file(FILE *file, unsigned *size);
 #endif  // !HASH_H
--- P02D13/src/hash.c
+++ P02D13/src/hash.c
@@ -15,39 +15,84 @@ void create_hash() {
   free(message);
 }
 void create_bin_arr(unsigned **message) {
-  unsigned i = 0;
-  char path[100] = "src/message.txt";
+  char path[60] = "src/message.txt";
   FILE *file = fopen(path, "r");
   if (file != NULL) {
-    do {
-      message[i] = (unsigned *)malloc(1 * sizeof(unsigned));
-      message[i][0] = 0;
-      while (!feof(file)) {
-        char ch;
-        if (fscanf(file, "%c", &ch) != EOF) {
-          message[i][0] <<= 8;
-          message[i][0] += (unsigned)ch;
-          printf("%u ", message[i][0]);
+    unsigned i, j, k, size, tmp1, tmp2 /*tmp3*/;
+    char ch;
+    // create dynamic arr by 512bit: unsigned arr[][16]
+    get_size_of_binary_file(file, &size);
+    tmp1 = size % 64;
+    tmp2 = size / 64 + 1;
+    message = (unsigned **)realloc(message, tmp2 * sizeof(unsigned *));
+    for (i = 0; i < tmp2; i++) {
+      message[i] = (unsigned *)malloc(16 * sizeof(unsigned));
+      // if (tmp2 - i != 1) {
+      //   tmp3 = 64;
+      // } else {
+      //   tmp3 = tmp1;
+      // }
+      for (k = 0, j = -1; k < 64; k++) {
+        if (k % 4 == 0) {
+          j++;
+          message[i][j] = 0;
         }
+        if ((tmp2 - i != 1) || k < tmp1) {
+          fscanf(file, "%c", &ch);
+          message[i][j] <<= 8;
+          message[i][j] += (unsigned)ch;
+        } else if (k == tmp1) {
+          message[i][j] <<= 8;
+          message[i][j] += 1 << 7;
+        }
+        // FIX !!! NEED PASTE LENGTH OF FILE (NOT ZERO AFTER 56TH BYTE)
+        else {
+          message[i][j] <<= 8;
+        }
+        printf("%x ", message[i][j]);
       }
-      i++;
-    } while (i < 1);  // create dynamic arr[1][512]
+    }
+    for (i = 0; i < tmp2; i++) {
+      for (j = 0; j < 16; j++) {
+        printf("%08x ", message[i][j]);
+      }
+    }
+
+    if (tmp1 >= 56) {
+      tmp2 += 1;
+      message = (unsigned **)realloc(message, tmp2 * sizeof(unsigned *));
+      message[tmp2 - 1] = (unsigned *)malloc(16 * sizeof(unsigned));
+      for (j = 0; j < 56; j++) {
+        message[tmp2 - 1][j] = 0;
+      }
+    }
+
+    // add length of file
+    // for (j = 0; j < 56; j++) {
+    //   message[tmp2 - 1][j] = 0;
+    // }
+
+    // if len of matrix (in binary) < 448 then add 1 and puts zero to end of
+    // 447th count len matrix and write this at 64 bit add this 64bit to end
+    // of arr (from 448th to 511th)
     fclose(file);
-    for (unsigned k = 0; k < i; k++) {
-      free(message[k]);
+    for (i = 0; i < tmp2; i++) {
+      free(message[i]);
     }
   } else {
     printf("n/a");
   }
-  // begin read matrix
-  // coding each symbol to binary format
-  // if len of matrix (in binary) < 448 then add 1 and puts zero to end of 447th
-  // count len matrix and write this at 64 bit
-  // add this 64bit to end of arr (from 448th to 511th)
+}
+// size of file <= 4 Gb
+void get_size_of_binary_file(FILE *file, unsigned *size) {
+  fseek(file, 0, SEEK_END);
+  *size = ftell(file);
+  printf("%u\n", *size);
+  fseek(file, 0, SEEK_SET);
 }
 void create_h_arr(unsigned *h_arr, int n) {
-  // first 32 bit of fractional part square root of first 8 simple numbers [from
-  // 2 to 19]
+  // first 32 bit of fractional part square root of first 8 simple numbers
+  // [from 2 to 19]
   create_arr_simple_num(h_arr, n);
   for (int i = 0; i < n; i++) {
     double num = sqrt(h_arr[i]);
@@ -56,8 +101,8 @@ void create_h_arr(unsigned *h_arr, int n) {
   }
 }
 void create_k_arr(unsigned *k_arr, int n) {
-  // first 32 bit of fractional part cube root of first 64 simple numbers [from
-  // 2 to 311]
+  // first 32 bit of fractional part cube root of first 64 simple numbers
+  // [from 2 to 311]
   create_arr_simple_num(k_arr, n);
   for (int i = 0; i < n; i++) {
     double num = cbrt(k_arr[i]);
@@ -94,16 +139,5 @@ void create_arr_simple_num(unsigned *h_arr, int n) {
       }
     } while (flag_simple == 0);
     h_arr[i] = num;
-  }
-}
-void get_size_of_binary_file(char *path, long int *size, int *err) {
-  FILE *file;
-  if ((file = fopen(path, "rb")) != NULL) {
-    fseek(file, 0, SEEK_END);
-    *size = ftell(file) / sizeof(ROWS);
-    printf("%ld\n", ftell(file));
-    fclose(file);
-  } else {
-    *err = 1;
   }
 }
